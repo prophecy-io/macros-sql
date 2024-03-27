4ffc77597bcc92e92ccc914fec5c567d79f0f170
Rohit Jain <jain.rohit625@gmail.com>
2024-03-27 16:10:42 +0530

    removed definition from spec

--- gems/prophecy_macro_sql/macros/deduplicate_complete.py
+++ gems/prophecy_macro_sql/macros/deduplicate_complete.py
@@ -105,30 +105,3 @@ class Deduplicate(MacroSpec):
         else:
             resolved_macro_name = macro_name
         return f'{{{{ {resolved_macro_name}({arguments}) }}}}'
-
-    def definition(self, props: DeduplicateProperties) -> str:
-        sql = """
-            {%- macro deduplicate(relation, partition_by, order_by) -%}
-
-                with row_numbered as (
-                    select
-                        _inner.*,
-                        row_number() over (
-                            partition by {{ partition_by }}
-                            order by {{ order_by }}
-                        ) as rn
-                    from {{ relation }} as _inner
-                )
-                select
-                    distinct data.*
-                from {{ relation }} as data
-                natural join row_numbered
-                where row_numbered.rn = 1
-        {%- endmacro -%}
-        
-        {%- macro deduplicate_simple(relation) -%}
-                select
-                    distinct * from {{relation}}
-        {%- endmacro -%}
-        """
-        return sql
